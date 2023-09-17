4754b638aa58434ebf7392148333561cbcca5581
hshuong <hshuong7978@yahoo.com>
2023-09-18 06:27:44 +0700

    bai moi safe args

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,17 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("androidx.navigation.safeargs")
 }
 
 android {
     namespace = "com.hfad.secretmessage"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.hfad.secretmessage"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -33,15 +34,27 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildToolsVersion = "34.0.0"
 }
 
 dependencies {
+    val navVersion = "2.7.2"
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Navigation Component
+    implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
+   // implementation("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
+//    implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
+//    // Feature module Support
+//    implementation("androidx.navigation:navigation-dynamic-features-fragment:$navVersion")
+//    // Jetpack Compose Integration
+//    implementation("androidx.navigation:navigation-compose:$navVersion")
+
 }
--- app/src/main/java/com/hfad/secretmessage/MessageFragment.kt
+++ app/src/main/java/com/hfad/secretmessage/MessageFragment.kt
@@ -8,17 +8,18 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.findNavController
 
-class WelcomeFragment : Fragment() {
-
+class MessageFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?): View? {
+
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_welcome, container, false)
-        val startButton = view.findViewById<Button>(R.id.start)
-        startButton.setOnClickListener {
-            view.findNavController().navigate(R.id.action_welcomeFragment_to_messageFragment)
+        val view = inflater.inflate(R.layout.fragment_message, container, false)
+        val nextButton = view.findViewById<Button>(R.id.next)
+        nextButton.setOnClickListener {
+            view.findNavController().navigate(R.id.action_messageFragment_to_encryptFragment)
         }
+        // Inflate the layout for this fragment
         return view
     }
 
